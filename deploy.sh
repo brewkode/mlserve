5b09071572762587f42d3943f8904665bf8e788c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-12-08 20:44:51 +0530

    Sample YAML spec + first cut of deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Step 0. Read the YAML file and compute the required properties for deployment
+## Step 1. Render the app.json
+## Step 2. Deploy the application
+## Step 3. UPSERT the DNS Name on Route53
