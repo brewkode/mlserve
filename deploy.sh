cdb45ceeb4ff68faa24fb8b38a7ca305e5d1a55c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-12-08 20:47:18 +0530

    Adding y2j

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,7 @@
 ## Step 1. Render the app.json
 ## Step 2. Deploy the application
 ## Step 3. UPSERT the DNS Name on Route53
+
+y2j() {
+  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'
+}
